f24ec0e3935f0f6eb7c0ae9807e66c23c5b46100
Rayan Al Mohaize <rayan.almohaize@gmail.com>
2024-04-08 12:01:57 +0200

    seuillage

--- kpp.py
+++ kpp.py
@@ -1,104 +1,73 @@
-import json
-import numpy as np
+# import necessary packages
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import  train_test_split
+from sklearn.metrics import classification_report
+from pyimagesearch import simplepreprocessor
+from pyimagesearch import simpledatasetloader
+from imutils import paths
 import os
+import glob
+import cv2
+import numpy as np
 
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
 
-# Charger les données annotées à partir des fichiers JSON
-def charger_donnees_de_dossier(chemin_dossier):
-    X = []  # Caractéristiques
-    y = []  # Étiquettes
+    return allFiles
 
-    for nom_fichier in os.listdir(chemin_dossier):
-        if nom_fichier.endswith('.json'):
-            with open(os.path.join(chemin_dossier, nom_fichier), 'r') as f:
-                donnees = json.load(f)
-                for forme in donnees['shapes']:
-                    # Exemple: annotation = {'label': '2 euros', 'bbox': [x, y, width, height]}
-                    etiquette = forme['label']
-                    caractéristique = extraire_caractéristique(forme)  
-                    X.append(caractéristique)
-                    y.append(etiquette)
-                
-    return np.array(X), np.array(y)
+imagePaths = getListOfFiles("./datasets/") ## Folder structure: datasets --> sub-folders with labels name
+#print(imagePaths)
 
-def extraire_caractéristique(annotation):
-    """
-    Extrait la valeur de la pièce à partir du fichier JSON annoté par Labelme.
-    
-    Parameters:
-    annotation (dict): Données d'annotation.
-    
-    Returns:
-    float: La valeur de la pièce annotée dans le fichier JSON.
-    """
-    # Extraire la valeur de la pièce à partir de l'annotation
-    etiquette_piece = annotation['label']
-    
-    montants_pieces = {
-    '2 euro': 2.0,
-    '1 euro': 1.0,
-    '50 centime': 0.50,
-    '20 centime': 0.20,
-    '10 centime': 0.10,
-    '5 centime': 0.05,
-    '2 centime': 0.02,
-    '1 centime': 0.01,
-    }
+data = []
+lables = []
+c = 0 ## to see the progress
+for image in imagePaths:
 
-    # Rechercher la sous-chaîne indiquant le montant de la pièce
-    # Par exemple, si l'étiquette est "pièce de 50 centimes", extraire "50 centime"
-    valeur_piece = None
-    for cle in montants_pieces:
-        if cle in etiquette_piece:
-            valeur_piece = cle
-            break
+    lable = os.path.split(os.path.split(image)[0])[1]
+    lables.append(lable)
 
-    # Si la valeur de la pièce est trouvée dans le dictionnaire, retourner sa valeur numérique
-    if valeur_piece is not None:
-        return montants_pieces[valeur_piece]
-    else:
-        # Gérer le cas où la valeur de la pièce n'est pas trouvée
-        return None  # ou toute valeur par défaut que vous souhaitez
+    img = cv2.imread(image)
+    img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_AREA)
+    data.append(img)
+    c=c+1
+    print(c)
 
-def extraire_caractéristiques_de_image(image):
-    image_redimensionnee = image.resize((100, 100)).convert('L')
-    
-    # Convertir l'image en un tableau numpy
-    tableau_image = np.array(image_redimensionnee)
-    
-    # Aplatir le tableau pour obtenir un vecteur de caractéristiques
-    caractéristiques = tableau_image.flatten()
-    
-    return caractéristiques
+#print(lables)
 
-# Chemin vers le dossier contenant les fichiers JSON annotés
-chemin_dossier = '/Users/rayanalmohaize/Documents/L3/image/image_entrainement'
+# encode the labels as integer
+data = np.array(data)
+lables = np.array(lables)
 
-# Charger les données à partir du dossier contenant les fichiers JSON
-X, y = charger_donnees_de_dossier(chemin_dossier)
+le = LabelEncoder()
+lables = le.fit_transform(lables)
 
-# Encoder les étiquettes en valeurs numériques
-encodeur_etiquettes = LabelEncoder()
-y_encodé = encodeur_etiquettes.fit_transform(y)
+myset = set(lables)
+print(myset)
 
-# Diviser les données en ensembles d'entraînement et de test
-X_entrainement, X_test, y_entrainement, y_test = train_test_split(X, y_encodé, test_size=0.2, random_state=42)
+dataset_size = data.shape[0]
+data = data.reshape(dataset_size,-1)
 
-# Remodeler les données d'entraînement et de test
-X_entrainement = X_entrainement.reshape(-1, 1)
-X_test = X_test.reshape(-1, 1)
+print(data.shape)
+print(lables.shape)
+print(dataset_size)
 
-# Initialiser et entraîner le modèle KNN
-modele_knn = KNeighborsClassifier(n_neighbors=8)  
-modele_knn.fit(X_entrainement, y_entrainement)
+(trainX, testX, trainY, testY ) = train_test_split(data, lables, test_size= 0.25, random_state=42)
 
-# Faire des prédictions sur l'ensemble de test
-y_pred = modele_knn.predict(X_test)
+model = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
+model.fit(trainX, trainY)
 
-# Calculer la précision du modèle
-precision = accuracy_score(y_test, y_pred)
-print("Précision:", precision)
+print(classification_report(testY, model.predict(testX), target_names=le.classes_))
