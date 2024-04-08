b5556d15e1c1ff96027b781c763adcd1a9d3b56d
Djibrildh <145924000+Djibrildh@users.noreply.github.com>
2024-04-08 10:59:58 +0200

    Add files via upload

--- Normalisation.py
+++ Normalisation.py
@@ -1,26 +1,32 @@
 import cv2
 import numpy as np
-import json
+import os 
 
-def normalize_image_from_json(json_file):
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    
-    image_path = data["imagePath"]
-    image = cv2.imread(image_path)
-    
-    if image is None:
-        print(f"Erreur : Impossible de charger l'image à partir de {image_path}")
-        return
-    
-    if len(image.shape) > 2 and image.shape[2] > 1:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    normalized_image = image / 255.0
-    
-    cv2.imshow("Image normalisée", normalized_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def normalize_image(image_folder, output_folder):
 
-testJsonPath = "json_file/0.json"
-normalize_image_from_json(testJsonPath) 
+    if not os.path.exists(output_folder) :
+        os.makedirs(output_folder)
+
+
+    listeImg = os.listdir(image_folder)
+    for img_name in listeImg:
+        img_path = os.path.join(image_folder, img_name)
+        image = cv2.imread(img_path)
+        
+        if image is None:
+            print(f"Erreur : Impossible de charger l'image {img_path}")
+            continue
+        
+        if len(image.shape) > 2 and image.shape[2] > 1:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        normalized_image = image / 255.0
+
+        output_path = os.path.join(output_folder, img_name)
+        cv2.imwrite(output_path, normalized_image * 255)
+        
+        print(f"Image normalisée enregistrée sous : {output_path}")
+
+folderPath = "imgTrain"
+output_folder = "images_Normalisées"
+normalize_image(folderPath, output_folder)
