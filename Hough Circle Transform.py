eb6b9341c2ec3a04c44b2ce5e39d7c36a20aec06
Djibrildh <145924000+Djibrildh@users.noreply.github.com>
2024-04-18 11:44:45 +0200

    Add files via upload

---
+++ Hough Circle Transform.py
@@ -0,0 +1,55 @@
+import sys
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from pretraitement import find_best_threshold, normalize_image, threshold_image
+
+def main(argv):
+    default_file = '149.jpg'
+    filename = argv[0] if len(argv) > 0 else default_file
+    # Loads an image
+    src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
+        return -1
+
+    # Appliquer le prétraitement à l'image
+    normalized_image = normalize_image(src)
+    if normalized_image is None:
+        return -1
+
+    # Convertir l'image normalisée en niveaux de gris
+    gray = cv.cvtColor(normalized_image, cv.COLOR_BGR2GRAY)
+    gray = cv.medianBlur(gray, 5)
+
+    # Appliquer le seuillage à l'image en niveaux de gris
+    threshold_value = 100 # Valeur de seuil à ajuster selon vos besoins
+    ret, thresholded_image = cv.threshold(gray, threshold_value, 255, cv.THRESH_BINARY)
+
+    rows = gray.shape[0]
+    circles = cv.HoughCircles(thresholded_image, cv.HOUGH_GRADIENT, 1, rows / 8,
+                              param1=100, param2=30,
+                              minRadius=1, maxRadius=30)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(src, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(src, center, radius, (255, 0, 255), 3)
+
+    cv.imshow("detected circles", src)
+    cv.waitKey(0)
+
+    return 0
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
